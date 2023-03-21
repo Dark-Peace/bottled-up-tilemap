5c2514e3461951cecb8f24286f6e995db1250208
Dark Peace <74102789+Dark-Peace@users.noreply.github.com>
2023-03-21 20:11:53 +0100

    Group fav+hide+dupli+delete+search

--- BottledTileMap/TilePalette/TilePalette.gd
+++ BottledTileMap/TilePalette/TilePalette.gd
@@ -17,12 +17,12 @@ const GROUPCHAR_TILEMAP:String = "$"
 const GROUPCHAR_TILESET:String = "€"
 const GROUPCHAR_TEXTURE:String = "£"
 const GROUPCHAR_CUSTOM:String = "#"
-const GROUPNAME_ALLTILES:String = "ALL_TILES"
-const GROUPNAME_NOGROUP:String = "NO_GROUP"
+const GROUPNAME_ALLTILES:String = "ALL TILES"
+const GROUPNAME_NOGROUP:String = "NO GROUP"
 
 #var _tile_list: ItemList
 @onready var _panel:ScrollContainer = $"%Panel"
-@onready var _group_tree:Tree = $"%GroupTree"
+@onready var _group_tree = $"%GroupTree"
 @onready var _preview:ScrollContainer = $"%Preview"
 @onready var _preview_list:VBoxContainer = $"%PreviewList"
 
@@ -30,13 +30,12 @@ var _tile_map_editor: Control
 var tilemap:BottledTileMap
 var curr_preview_type:String
 #var can_see_tile_hint = false
-enum VIEW {Group, Tex, List}
-var current_view = VIEW.Group
 
 var tile_list:Dictionary
 var group_list:Dictionary
 var fav_groups:Array[String]
 var hidden_groups:Array[String]
+var curr_group:String
 
 var can_multi_check:bool = false
 var can_show_subview:bool = true
@@ -47,16 +46,13 @@ var lock_dock:bool = false
 
 var tileset:TileSet : set = _set_tileset
 func _set_tileset(value):
-	if tileset == value:
+	if value == null or tileset == value:
 		return
-#	_clear()
 	tileset = value
 	if tileset: _fill()
 
 func set_lists(tile_list: ItemList, subtile_list: ItemList):
 	pass
-#	_tile_list = tile_list
-#	_subtile_list = subtile_list
 
 func set_tools(tile_map_editor: Control, interface_display_scale: float = 1):
 	_tile_map_editor = tile_map_editor
@@ -214,38 +210,113 @@ func _find_in_editor(target:String="TileMapEditor", node:Node=get_tree().root, _
 
 func _ready():
 	BTM.palette = self
-	fav_groups.append(GROUPNAME_NOGROUP)
-	fav_groups.append(GROUPNAME_ALLTILES)
+#	fav_groups.append(GROUPNAME_NOGROUP)
+#	fav_groups.append(GROUPNAME_ALLTILES)
+	
+	$"%GroupTree".get_child(0).connect("pressed", select_group.bind(GROUPNAME_ALLTILES))
+	$"%GroupTree".get_child(1).connect("pressed", select_group.bind(GROUPNAME_NOGROUP))
 #	get_parent().resized.connect(resize)
 #
 #func resize():
-#	print("ok")
 #	size = get_parent().size
 
 func _process(delta):
 	if not active: return
 	size = get_parent().size
 
+
+
+
+
 func _fill():
-	_clear()
+	_clear_group_view()
 #	tilemap.tilecell = get_tilecell()
 	if not tileset.has_meta("TileList"):
 		tileset.set_meta("TileList", BTM.get_tiles_ids(tileset))
 	_fill_group_view()
 	_group_tree.show()
 
-func _fill_list_view(tilelist:Array[BTM.TILEID]=tileset.get_meta("TileList")):
+func _on_togglefav_pressed(fav=null):
+	if fav == null: fav = $"%Togglefav".button_pressed
+	for g in _group_tree.get_children():
+		if fav: g.visible = (g.text in fav_groups or g.text in [GROUPNAME_NOGROUP, GROUPNAME_ALLTILES])
+		else: g.visible = not g.text in hidden_groups
+
+func _fill_group_view():
+	if not tileset: return
+	var groups:Dictionary = tileset.get_meta("groups_by_groups", {})
+	
+	$"%ALL TILES".set_meta("TILES", tileset.get_meta("TileList"))
+	update_no_group()
+	group_list[GROUPNAME_ALLTILES] = $"%ALL TILES"
+	group_list[GROUPNAME_NOGROUP] = $"%NO GROUP"
+	
+	var tilelist:Array[BTM.TILEID]
+	for g in groups.keys():
+		tilelist = tilemap.EMPTY_TILE_ARRAY as Array
+		create_group(g, groups.get(g, tilelist))
+	
+	if curr_group == "":
+		$"%ALL TILES".button_pressed = true
+		select_group(GROUPNAME_ALLTILES)
+
+func update_no_group():
+	$"%NO GROUP".set_meta("TILES", get_tiles_without_groups())
+	print($"%NO GROUP".get_meta("TILES"))
+
+func get_tiles_without_groups():
+	var no_group:Array[BTM.TILEID]
+	for id in tileset.get_meta("TileList"):
+		if tilemap.tile_has_any_group(id.v): continue
+		no_group.append(id)
+	return no_group
+	
+func create_group(g:String, group_content:Array, tree=_group_tree):
+	var group:Button = _group_tree.get_child(0).duplicate()
+	
+	group.text = g
+	group.name = g
+	group.set_meta("TILES", group_content)
+	group_list[g] = group
+	if g == tilemap.dock_group_selected:
+		group.button_pressed = true
+		select_group(g)
+		return true
+	if group.pressed.is_connected(select_group.bind(GROUPNAME_ALLTILES)): group.pressed.disconnect(select_group.bind(GROUPNAME_ALLTILES))
+	group.pressed.connect(select_group.bind(g))
+	_group_tree.add_child(group)
+	return false
+
+func select_group(group:String,fav_tree:bool=false):
+	curr_group = group
+	for n in _group_tree.get_children():
+		if n.name == group:
+			n.button_pressed = true
+			continue
+		n.button_pressed = false
+	tilemap.dock_group_selected = group
+	_fill_list_view(_group_tree.get_node(group).get_meta("TILES",tilemap.EMPTY_TILE_ARRAY))
+
+func _fill_list_view(tilelist:Array=tileset.get_meta("TileList")):
 	$"%ListView".clear()
 	var id_map = tileset.get_meta("ID_Map", {})
-	var tilelist_indexes:Dictionary; var index:int=0; var list_index:int
+	var tilelist_indexes:Dictionary; var index:int=0; var list_index:int; var unique_id:String
 	for id in tilelist:
-		tilelist_indexes[index] = id
+		if not id is Vector3i: tilelist_indexes[index] = id
+		else:
+			id = BTM.TILEID.new(id.z, Vector2i(id.x,id.y))
+			tilelist_indexes[index] = id
 		index += 1
 		$"%ListView".add_item("", tileset.get_source(id.source).texture)
 		list_index = $"%ListView".get_item_count()-1
 		$"%ListView".set_item_icon_region(list_index,tileset.get_source(id.source).get_tile_texture_region(id.coords))
 		$"%ListView".set_item_tooltip_enabled(list_index,true)
-		$"%ListView".set_item_tooltip(list_index, str(tilemap._get_id_in_map(id.v)))
+		unique_id = str(tilemap._get_id_in_map(id.v))
+		$"%ListView".set_item_tooltip(list_index, unique_id)
+		if unique_id == tilemap.dock_tile_selected:
+			$"%ListView".select(index-1)
+			_on_ListView_item_selected(index-1)
+	if not $"%ListView".is_anything_selected(): $"%ListView".select(0)
 	tileset.set_meta("TileListIndexes", tilelist_indexes)
 
 func _on_sub_tab_tab_changed(tab):
@@ -254,12 +325,8 @@ func _on_sub_tab_tab_changed(tab):
 func _fill_subtile_view():
 	pass
 
-func _fill_variant_view():
-	pass
-
 func _fill_alttile_view():
 	var current_tile:BTM.TILEID = tilemap.current_tile
-	
 	var region:Rect2; var alt:TileData;
 	for a in 8:
 		alt = tileset.get_source(current_tile.source).get_tile_data(current_tile.coords, a)
@@ -275,63 +342,6 @@ func _fill_subview():
 	match $"%SubTab".current_tab:
 		0: _fill_subtile_view()
 		1: _fill_alttile_view()
-		2: _fill_variant_view()
-
-func _fill_group_view():
-	if not tileset: return
-	_group_tree.clear()
-	_group_tree.create_item()
-	var groups:Dictionary = tilemap.get_meta("groups_by_groups", {})
-	
-	var no_group:Array[BTM.TILEID] = get_tiles_without_groups()
-	var tilelist:Array[BTM.TILEID] = tileset.get_meta("TileList")
-	var all_tiles = create_group("ALL_TILES", tilelist)
-#	_group_tree.set_selected(all_tiles, 0)
-	create_group("NO_GROUP", no_group)
-	
-	for g in groups.keys():
-		tilelist = tilemap.EMPTY_TILE_ARRAY as Array[BTM.TILEID]
-		create_group(g, groups.get(g, tilelist))
-	
-	fill_fav_group()
-	select_group()
-
-func get_tiles_without_groups():
-	var no_group:Array[BTM.TILEID]
-	for id in tileset.get_meta("TileList"):
-		if id in tilemap.get_meta("groups_by_ids", {}).keys(): continue
-		no_group.append(id)
-	return no_group
-
-func fill_fav_group():
-	$"%Favorites".clear()
-	$"%Favorites".create_item()
-	for g in fav_groups:
-		print(g)
-		create_group(g, tilemap.EMPTY_TILE_ARRAY, $"%Favorites")
-
-func create_group(g:String, group_content:Array[BTM.TILEID], tree=_group_tree):
-	var group:TreeItem = tree.create_item(tree.get_root())
-	group.set_icon_max_width(0,16)
-	group.set_text(0, g)
-	group.set_meta("TILES", group_content)
-	if not group_content.is_empty():
-		group.set_icon(0, tileset.get_source(group_content[0].source).texture)
-		group.set_icon_region(0, tileset.get_source(group_content[0].source).get_tile_texture_region(group_content[0].coords))
-	if tree == _group_tree:
-		group_list[g] = group
-	return group
-
-func select_group(fav_tree:bool=false):
-	var curr_group
-	if not fav_tree:
-		curr_group = _group_tree.get_selected().get_text(0)
-		if curr_group in fav_groups and $"%Favorites".get_root() != null:
-			$"%Favorites".set_selected(get_group_item(curr_group, $"%Favorites"),0)
-	else:
-		curr_group = $"%Favorites".get_selected()
-		_group_tree.set_selected(get_group_item(curr_group.get_text(0), _group_tree),0)
-	_fill_list_view(_group_tree.get_selected().get_meta("TILES",tilemap.EMPTY_TILE_ARRAY))
 
 func get_group_item(n:String, tree:Tree):
 	var children:Array[TreeItem] = tree.get_root().get_children()
@@ -341,28 +351,30 @@ func get_group_item(n:String, tree:Tree):
 		if child.get_text(0) == n: return child
 		children.append_array(child.get_children())
 
-func _clear():
-	_clear_group_view()
-
 func _clear_list_view():
 	$"%ListView".clear()
 
 func _clear_group_view():
+	for n in _group_tree.get_children():
+		if n.name in [GROUPNAME_ALLTILES,GROUPNAME_NOGROUP]: continue
+		n.queue_free()
+	group_list.clear()
 	_clear_list_view()
 
 func _on_delete_group_pressed():
-	var curr:TreeItem = _group_tree.get_selected()
-	var curr_name:String = curr.get_text(0)
-	if curr_name in [GROUPNAME_ALLTILES, GROUPNAME_NOGROUP]:
-		push_warning("Not allowed to remove the group "+curr_name)
+	if curr_group in [GROUPNAME_ALLTILES, GROUPNAME_NOGROUP]:
+		push_warning("Not allowed to remove the group "+curr_group)
 		return
 	
-	for id in tilemap.get_meta("groups_by_groups", {})[curr_name]:
-		tilemap.get_meta("groups_by_ids", {})[id].erase(curr_name)
-	tilemap.get_meta("groups_by_groups", {}).erase(curr_name)
+	var list = tilemap.get_tiles_in_group(curr_group).duplicate()
+	for id in list:
+		tilemap.remove_group_from_tile(id, curr_group)
+		tilemap.remove_tile_from_group(id, curr_group)
 	
-	_group_tree.get_selected().free()
-	_fill()
+	_group_tree.get_node(curr_group).queue_free()
+#	_clear_list_view()
+	select_group(GROUPNAME_ALLTILES)
+	update_no_group()
 
 func _check_remove_fav_group(curr:String):
 	if not curr in fav_groups: return
@@ -370,56 +382,55 @@ func _check_remove_fav_group(curr:String):
 		push_warning("Not allowed to unfavorite the group "+curr)
 		return
 	fav_groups.erase(curr)
-	$"%Favorites".get_selected().free()
-	fill_fav_group()
-
-func _on_dupli_group_pressed():
-	var curr:TreeItem = _group_tree.get_selected()
-	var old_name:String = curr.get_text(0)
-	var new_name:String = old_name+"2"
-	var new_content:Array[BTM.TILEID]
-	
-	if old_name == "ALL_TILES": new_content = tileset.get_meta("TileList").duplicate()
-	elif old_name == "NO_GROUP": new_content = get_tiles_without_groups().duplicate()
-	else: new_content = tilemap.get_meta("groups_by_groups", {})[old_name].duplicate()
-	
-	tilemap.get_meta("groups_by_groups", {})[new_name] = new_content
-	for id in tilemap.get_meta("groups_by_groups", {})[new_name]:
-		if not id.isIN(tilemap.get_meta("groups_by_ids", {}).keys()): tilemap.get_meta("groups_by_ids", {})[id] = []
-		tilemap.get_meta("groups_by_ids", {})[id].append(new_name)
-	_fill()
 
 func _on_fav_group_pressed():
-	var curr:String = _group_tree.get_selected().get_text(0)
-	if not curr in fav_groups: fav_groups.append(curr)
-	else: _check_remove_fav_group(curr)
-	_fill()
+	if curr_group in hidden_groups:
+		push_warning("Can't favorite the group "+curr_group+" because it is a hidden group. Unhide it first.")
+		return
+	if not curr_group in fav_groups: fav_groups.append(curr_group)
+	else: _check_remove_fav_group(curr_group)
+
+func _on_dupli_group_pressed():
+	var new_name:String = curr_group+"2"
+	var new_content:Array[BTM.TILEID]
+	
+	while tilemap.group_exists(new_name): new_name += "2"
+	
+	if curr_group == GROUPNAME_ALLTILES: new_content = tileset.get_meta("TileList")
+	elif curr_group == GROUPNAME_NOGROUP: new_content = get_tiles_without_groups()
+	else: new_content = tileset.get_meta("groups_by_groups", {})[curr_group]
+	for id in new_content:
+		tilemap.add_tile_to_group(id.v, new_name)
+		tilemap.add_group_to_tile(id.v, new_name)
+	curr_group = new_name
+	create_group(new_name, new_content)
+	select_group(new_name)
+	update_no_group()
+#	_fill()
 
 func _on_hide_group_pressed():
-	var curr:String = _group_tree.get_selected().get_text(0)
-	_check_remove_fav_group(curr)
-	if curr in hidden_groups: hidden_groups.erase(curr)
-	elif curr in [GROUPNAME_ALLTILES, GROUPNAME_NOGROUP]:
-		push_warning("Not allowed to hide the group "+curr)
+	_check_remove_fav_group(curr_group)
+	if curr_group in hidden_groups: hidden_groups.erase(curr_group)
+	elif curr_group in [GROUPNAME_ALLTILES, GROUPNAME_NOGROUP]:
+		push_warning("Not allowed to hide the group "+curr_group)
 	else:
-		hidden_groups.append(curr)
-		_group_tree.get_selected().free()
-	_fill()
+		hidden_groups.append(curr_group)
+		_group_tree.get_node(curr_group).hide()
+	select_group(GROUPNAME_ALLTILES)
 
 func _on_tileset_changed(new_tileset: TileSet):
+	print("tileset")
 	_fill()
 
 func _on_ListView_item_selected(index: int) -> void:
 	if index != selected_right:
 		tilemap.current_tile = tileset.get_meta("TileListIndexes", {}).get(index, BTM.TILEID.new())
 		selected_left = index
+		tilemap.dock_tile_selected = $"%ListView".get_item_tooltip(index)
 	for item in $"%ListView".get_selected_items():
 		if item == selected_left or item == selected_right: continue
 		$"%ListView".deselect(item)
 	_fill_subview()
-#	current_tile = index
-#	_tile_list.select(current_tile)
-#	_tile_map_editor._palette_selected(int(current_tile))
 	# TODO : display subtile view
 #	init_tilecell()
 
@@ -456,18 +467,13 @@ func _on_TilePalette_resized() -> void:
 func get_tilecell():
 	return $"%TileCells"
 
-#func _on_Views_item_selected(index: int) -> void:
-#	current_view = index
-##	$HSplitContainer/TextureVBoxContainer/Main/HSplitContainer/Panel/GroupView.visible = current_view == VIEW.Group
-#	$HSplitContainer/TextureVBoxContainer/Main/HSplitContainer/Panel/ScalingHelper.visible = current_view in [VIEW.Tex, VIEW.List]
-#	$"%ListView".visible = current_view in [VIEW.Group, VIEW.List]
-#	_clear()
-#	_fill()
-
 func _on_Search_text_changed(new_text: String) -> void:
-	for g in group_list.keys():
-		if g in hidden_groups and not new_text[0] == GROUPCHAR_HIDDEN: continue
-		group_list[g].visible = new_text.is_subsequence_ofn(g)
+	for g in _group_tree.get_children():
+#		print(g.text)
+		if g.text in hidden_groups:
+			print(new_text[0])
+			g.visible = (new_text[0] == GROUPCHAR_HIDDEN and new_text.is_subsequence_ofn(GROUPCHAR_HIDDEN+g.text))
+		else: g.visible = new_text.is_subsequence_ofn(g.text)
 
 func _on_tab_changed(tab):
 	$TileMap.visible = (tab == 0)
@@ -486,7 +492,6 @@ func _on_tile_size_value_changed(value):
 
 func _on_toggle_sub_pressed():
 	can_show_subview = $"%ToggleSub".button_pressed
-
 
 func _on_clear_invalid_pressed():
 	tilemap.fix_invalid_tiles()
