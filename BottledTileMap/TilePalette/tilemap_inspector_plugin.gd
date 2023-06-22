a9a72038c7cb37c0394fe46afe53d27160c78440
Dark Peace <74102789+Dark-Peace@users.noreply.github.com>
2023-06-22 15:41:40 +0200

    update file

--- BottledTileMap/TilePalette/tilemap_inspector_plugin.gd
+++ BottledTileMap/TilePalette/tilemap_inspector_plugin.gd
@@ -1,4 +1,4 @@
-tool
+@tool
 extends EditorInspectorPlugin
 
 signal tile_map_tile_set_changed
@@ -12,10 +12,9 @@ func can_handle(object):
 	return object is TileMap
 
 func parse_begin(object):
-	if not _inspector.is_connected("property_edited", self, "_inspector_property_edited"):
-		_inspector.connect("property_edited", self, "_inspector_property_edited")
+	if _inspector.is_connected("property_edited",Callable(self,"_inspector_property_edited")): return
+	_inspector.connect("property_edited",Callable(self,"_inspector_property_edited"))
 
 func _inspector_property_edited(property):
-#	print("TilemapInspectorPlugin._on_tile_set_changed")
-	if property == "tile_set":
-		emit_signal("tile_map_tile_set_changed")
+	if property != "tile_set": return
+	emit_signal("tile_map_tile_set_changed")
